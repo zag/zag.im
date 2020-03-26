fab68ca2678ca77e390d029f3d604f0a5d3e7198
Aliaksandr Zahatski <github.send@zag.ru>
2020-03-26 17:23:41 +0300

    fix space

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   pathPrefix: '/',
   title: 'Alexandr Zahatski/Александр Загацкий | консалтинг ИТ проектов, функциональное прототипирование, создание MVP, решения на базе открытых технологий',
   subtitle: 'Интересуюсь персональным издательством и системами знаний, децентрализованными системами, а также инженерией программного обеспечения 💻. Оказываю услуги по разработке ПО.',
-  copyright: '© All rights reserved. 2002-2020.<a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>.<a href="https://zag.ru/2020/3/25/1/isxodniki-etogo-bloga">Made with 🌶️ and pod6</a>',
+  copyright: '© All rights reserved. 2002-2020. <a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. <a href="https://zag.ru/2020/3/25/1/isxodniki-etogo-bloga">Made with 🌶️ and pod6</a>',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-2388080-1',
